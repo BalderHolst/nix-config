f868e1a3a5b9d32a797d3fec2af4d2bd88169117
BalderHolst <balderwh@gmail.com>
2023-08-22 10:18:28 +0200

    Shell util script folder

--- home.nix
+++ home.nix
@@ -71,54 +71,11 @@ rec {
     '';
 
     # Kitty config
-    ".config/kitty/kitty.conf".text = ''
-        background_opacity 0.6
-        font_size 12.0
-        font_family FiraCode Nerd Font
-        
-        enable_audio_bell no
-        confirm_os_window_close 0
-        
-        shell zsh
-        
-        foreground #c0caf5
-        selection_background #33467C
-        selection_foreground #c0caf5
-        url_color #73daca
-        cursor #c0caf5
-        
-        # Tabs
-        active_tab_background #7aa2f7
-        active_tab_foreground #1f2335
-        inactive_tab_background #292e42
-        inactive_tab_foreground #545c7e
-        #tab_bar_bkground #15161E
-        
-        # normal
-        color0 #15161E
-        color1 #f7768e
-        color2 #9ece6a
-        color3 #e0af68
-        color4 #7aa2f7
-        color5 #bb9af7
-        color6 #7dcfff
-        color7 #a9b1d6
-        
-        # bright
-        color8 #414868
-        color9 #f7768e
-        color10 #9ece6a
-        color11 #e0af68
-        color12 #7aa2f7
-        color13 #bb9af7
-        color14 #7dcfff
-        color15 #c0caf5
-        
-        # extendedolors
-        color16 #ff9e64
-        color17 #db4b4b
-    '';
-        
+    ".config/kitty/kitty.conf".source = ./configs/kitty.conf;
+
+    # Utility scripts
+    ".myutils".source = ./utils;
+
     };
 
     programs.git = {
@@ -191,31 +148,8 @@ rec {
 
         source ~/.p10k.zsh # Initialize powerlevel10k prompt
 
-        # Archive Extraction
-        ex ()
-        {
-          if [ -f "$1" ] ; then
-            case $1 in
-              *.tar.bz2)   tar xjf $1   ;;
-              *.tar.gz)    tar xzf $1   ;;
-              *.bz2)       bunzip2 $1   ;;
-              *.rar)       unrar x $1   ;;
-              *.gz)        gunzip $1    ;;
-              *.tar)       tar xf $1    ;;
-              *.tbz2)      tar xjf $1   ;;
-              *.tgz)       tar xzf $1   ;;
-              *.zip)       unzip $1     ;;
-              *.Z)         uncompress $1;;
-              *.7z)        7z x $1      ;;
-              *.deb)       ar x $1      ;;
-              *.tar.xz)    tar xf $1    ;;
-              *.tar.zst)   unzstd $1    ;;
-              *)           echo "'$1' cannot be extracted via ex()" ;;
-            esac
-          else
-            echo "'$1' is not a valid file"
-          fi
-        }
+        # Add utils to PATH
+        PATH="$PATH"":${home_dir}/.myutils"
         '';
     };
 
