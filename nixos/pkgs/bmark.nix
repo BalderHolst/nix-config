98135e417bd6ba88cfc583b58b43d7cf9d515bb3
BalderHolst <balderwh@gmail.com>
2023-09-06 21:18:15 +0200

    Update bmark

--- nixos/pkgs/bmark.nix
+++ nixos/pkgs/bmark.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "bmark";
-  version = "0.1.4";
+  version = "0.1.5";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-wmuYPLG67G9ciZKc0dCIBWDOWYaN02sIvU2pOpveBSg=";
+    sha256 = "sha256-U1oJMQ+3pkjm1PSc98l2p+UKv2Z933xntl07TibbQ9A=";
   };
 
-  cargoHash = "sha256-14iRsvQwb/TcxrD7FI6iAQnwohVUIUQiosBvn72YCdU=";
+  cargoHash = "sha256-KzvfsNNvcXeHu4enyI8un+mx4dcu65F3qL4noxKBMEw=";
   cargoDepsName = pname;
 }
