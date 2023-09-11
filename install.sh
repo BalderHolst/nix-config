d603c33a58dfe3f9b31a18e626898ca0d5fa0c1c
BalderHolst <balderwh@gmail.com>
2023-09-11 20:30:04 +0200

    enable nix-command

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p git home-manager
+#!nix-shell -i bash -p git home-manager --extra-experimental-features nix-command
 
 stcolor="\u001b[34;1m"
 scolor="\u001b[32;1m"
