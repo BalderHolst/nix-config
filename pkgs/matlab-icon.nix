8971b99cebfac1327c9ad5559a09cad67278de66
BalderHolst <balderwh@gmail.com>
2023-12-17 14:49:19 +0100

    new matlab-icon hash

--- pkgs/matlab-icon.nix
+++ pkgs/matlab-icon.nix
@@ -6,6 +6,6 @@ makeDesktopItem {
     exec = "${userHome}/.config/home-manager/utils/matlab -desktop";
     icon = builtins.fetchurl {
         url = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Matlab_Logo.png/667px-Matlab_Logo.png";
-        sha256 = "sha256:02xrimzr62nxwysbirxgbsr030a6n4cji1vffx2cziax64g2f39q";
+        sha256 = "sha256:1379d2pdm49hcq9gib47jd7jrp2926h69an5jjgfh60rwnhgrslx";
     };
 }
