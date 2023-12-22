4c31ca9daac770bc92fe45f672110be808f92e8a
BalderHolst <balderwh@gmail.com>
2023-12-22 19:39:43 +0100

    revert to 23.05 to fix mathematica and steam

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Balder's System Flake";
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs-stable, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
     username = "balder";
@@ -11,7 +11,7 @@
     profile = import ./profile.nix;
 
     # configure pkgs
-    pkgs = import nixpkgs {
+    home-pkgs = import nixpkgs {
         inherit system;
         config = {
             allowUnfree = true;
@@ -19,23 +19,31 @@
         };
         overlays = [
             (final: prev: {
-                bmark = pkgs.callPackage pkgs/bmark.nix { };
-                blatex = pkgs.callPackage pkgs/blatex.nix { };
-                pyprland = pkgs.callPackage pkgs/pyprland.nix { };
-                matlab-icon = pkgs.callPackage pkgs/matlab-icon.nix { userHome = "/home/${username}"; };
-                mathematica-icon = pkgs.callPackage pkgs/mathematica-icon.nix { mathematicaPath = "${configDir}/impure/mathematica/result/bin/mathematica"; };
+                bmark = prev.callPackage pkgs/bmark.nix { };
+                blatex = prev.callPackage pkgs/blatex.nix { };
+                pyprland = prev.callPackage pkgs/pyprland.nix { };
+                matlab-icon = prev.callPackage pkgs/matlab-icon.nix { userHome = "/home/${username}"; };
+                mathematica-icon = prev.callPackage pkgs/mathematica-icon.nix { mathematicaPath = "${configDir}/impure/mathematica/result/bin/mathematica"; };
             })
         ];
     };
 
+    system-pkgs = import nixpkgs-stable {
+        inherit system;
+        config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+        };
+    };
+
     # configure lib
-    lib = nixpkgs.lib;
+    lib = nixpkgs-stable.lib;
 
   in {
 
     homeConfigurations = {
         "${username}" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
+            pkgs = home-pkgs;
             modules = [ (./. + "/profiles"+("/"+profile)+"/home.nix") ]; # load home.nix from selected PROFILE
             extraSpecialArgs = {
                 inherit inputs;
@@ -49,6 +57,7 @@
     nixosConfigurations = {
         "system" = lib.nixosSystem {
             inherit system;
+            pkgs = system-pkgs;
             modules = [ (./. + "/profiles"+("/"+profile)+"/configuration.nix") ]; # load configuration.nix from selected PROFILE
             specialArgs = { inherit username; };
         };
@@ -57,10 +66,11 @@
 
     inputs = rec {
 
-        nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+        nixpkgs-stable.url = "github:NixOS/nixpkgs/23.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
         home-manager = {
-            url = "github:nix-community/home-manager/release-23.11";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
