4db1acd4cd2016c32f757fa82e387cfbae9fd3b7
BalderHolst <balderwh@gmail.com>
2024-02-08 13:37:37 +0100

    Move neovim dependencies to 'neovim' module

--- user/programs/dev-collection.nix
+++ user/programs/dev-collection.nix
@@ -14,7 +14,6 @@
         gf # gdb fontend
         gdb # debugger
         lldb_9 # another debugger
-        vscode-extensions.vadimcn.vscode-lldb # lldb vscode extension used in neovim
 
         python311 # Python interpreter
         python311Packages.bpython
@@ -22,14 +21,8 @@
         rustc # the rust compiler
         cargo # rust build toolchain
         rustfmt # rust formatter
-        rust-analyzer # lsp for rust
         clippy # rust linter
         cargo-expand # Expand macroes
-
-        nodejs # local javascript runtime, mainly for pyright lsp
-        nodePackages_latest.pyright # python lsp
-
-        lua-language-server # lsp for lua
     ];
 
     home.file = {
--- user/programs/neovim.nix
+++ user/programs/neovim.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
     programs.neovim = {
         enable = true;
@@ -9,4 +9,12 @@
     home.sessionVariables = {
         EDITOR = "nvim";
     };
+
+    home.packages = with pkgs; [
+        vscode-extensions.vadimcn.vscode-lldb # lldb vscode extension used in neovim
+        rust-analyzer # lsp for rust
+        nodejs # local javascript runtime, mainly for pyright lsp
+        nodePackages_latest.pyright # python lsp
+        lua-language-server # lsp for lua
+    ];
 }
