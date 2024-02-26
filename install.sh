7a83ec3e1cd2bd9a9751cfcc3480079bf2a0093e
BalderHolst <balderwh@gmail.com>
2024-02-26 16:40:44 +0100

    install.sh: fix clone bug

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ error () {
 if [ ! -d "$CONFIG_DIR" ]
 then
     status "Cloning configuration."
-    nix-shell -p git --command 'git clone git@github.com:BalderHolst/nix-config "$CONFIG_DIR"'
+    nix-shell -p git --command "git clone git@github.com:BalderHolst/nix-config '$CONFIG_DIR'"
 fi
 
 # ============= Submodules =============
