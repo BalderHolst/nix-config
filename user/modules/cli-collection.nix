248115c7703655eaf12f2d6a20888723f3e8bb6f
BalderHolst <balderwh@gmail.com>
2024-09-18 12:52:10 +0200

    add zoxide

--- user/modules/cli-collection.nix
+++ user/modules/cli-collection.nix
@@ -1,41 +1,54 @@
-{ pkgs, configDir, ... }:
+{ pkgs, configDir, username, ... }:
 {
-    home.packages = with pkgs; [
-        neofetch # fancy system info
-        tldr # shot and sweet command examples
-        zip # zip your files
-        unzip # unzip your files
-        todo # simple todo list
-        ripgrep # awesome grepping tool
-        jq # json parsing cli
-        bat # cat with colors
-        wl-clipboard # cli clipboard manipulation. Also needed for neovim.
-        bmark # my terminal bookmark manager
-        blatex # my latex document framework
-        fzf # fuzzy finder
-        graphviz # create graphs!
-        nurl # generate nix expressions from urls
-        usbutils # list usb devices
-        du-dust
-        nh
-    ];
 
-    home.sessionVariables = {
-        PAGER = "bat";
-        FLAKE = configDir;
+    programs = {
+        zoxide = {
+            enable = true;
+            enableBashIntegration= true;
+            enableZshIntegration = true;
+            enableFishIntegration= false;
+            enableNushellIntegration = false;
+        };
     };
 
-    home.file = {
 
-        # Bmark config
-        ".config/bmark/config.toml".text = ''
-        data_dir = "/home/balder/.local/share/bmark"
-        dmenu_cmd = "rofi -matching fuzzy -dmenu"
-        editor_cmd = "nvim"
-        terminal_cmd = "kitty --detach -d"
-        alias_prefix = "_"
-        display_sep = " : "
-        '';
+    home = {
+        packages = with pkgs; [
+            neofetch # fancy system info
+            tldr # shot and sweet command examples
+            zip # zip your files
+            unzip # unzip your files
+            todo # simple todo list
+            ripgrep # awesome grepping tool
+            jq # json parsing cli
+            bat # cat with colors
+            wl-clipboard # cli clipboard manipulation. Also needed for neovim.
+            bmark # my terminal bookmark manager
+            blatex # my latex document framework
+            fzf # fuzzy finder
+            graphviz # create graphs!
+            nurl # generate nix expressions from urls
+            usbutils # list usb devices
+            du-dust
+            nh
+        ];
 
+        sessionVariables = {
+            PAGER = "bat";
+            FLAKE = configDir;
+        };
+
+        file = {
+            # Bmark config
+            ".config/bmark/config.toml".text = ''
+                data_dir = "/home/balder/.local/share/bmark"
+                dmenu_cmd = "rofi -matching fuzzy -dmenu"
+                editor_cmd = "nvim"
+                terminal_cmd = "kitty --detach -d"
+                alias_prefix = "_"
+                display_sep = " : "
+                '';
+
+        };
     };
 }
