88025ce6c73e792af30b5cbddce196694813b70e
BalderHolst <balderwh@gmail.com>
2024-09-21 15:12:04 +0200

    Hint to use wayland

--- system/general.nix
+++ system/general.nix
@@ -90,8 +90,8 @@
     };
 
     environment.sessionVariables = {
-        MOZ_ENABLE_WAYLAND = "1"; # Hint firefox to use wayland
-        NIXOS_OZONE_WL = "0"; # Hint electron apps to use wayland
+        MOZ_ENABLE_WAYLAND = 1; # Hint firefox to use wayland
+        NIXOS_OZONE_WL = 1;     # Hint electron apps to use wayland
     };
 
     hardware = {
