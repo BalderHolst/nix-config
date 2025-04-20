483bdce0d7707ce6460aeccef0ffcd62c870d894
Balder W. Holst <balderwh@gmail.com>
2025-04-20 20:03:44 +0200

    update inputs

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
 
     inputs = {
 
-        nixpkgs-stable.url = "github:NixOS/nixpkgs/24.05";
+        nixpkgs-stable.url = "github:NixOS/nixpkgs/24.11";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
         home-manager = {
